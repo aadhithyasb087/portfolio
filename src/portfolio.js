4caa3bfe123053b9e0f157c8d7adb59f2e356054
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-07-08 23:56:25 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -81,7 +81,7 @@ React Native; React Component library: Material UI and Chakra UI.",
 "⚡ Backend: Node.js, Express.js and RESTful Api.",
 "⚡ Programming Languages: JavaScript, Typescript, Python (Intermediate) , C and Java (Basic).",
 "⚡ Clouds and Databases: SQL - MySQL, MongoDB - Mongoose and Firebase.",
-"⚡ Developer Tools: Socket.io, Git, Github, Jira, DevOps, VS Code, Postman and MS Ofiice",
+"⚡ Developer Tools: Socket.io, Git, Github, Jira, DevOps, VS Code, Postman, Canva, Figma and MS Ofiice",
 "⚡ Testing Tools: Unit- Jest, E2E- Playwright.",
 "⚡ Deployment Tools: Git, Vercel, Firebase and Render",
       ],
@@ -518,11 +518,11 @@ const achievements = {
       duration: "June 2023 - Nov 2023",
       location: "Coimbatore TamilNadu",
       description:
-        "• Collaborated on developing a GUI for Evaluation Module (EVM) for Texas Instruments.
-• Successfully built an LED driver GUI using Polymer.js and enabled users to connect and test hardware with various
+        "Collaborated on developing a GUI for Evaluation Module (EVM) for Texas Instruments.
+Successfully built an LED driver GUI using Polymer.js and enabled users to connect and test hardware with various
 LED configurations and modes, resulting in positive customer feedback and increased client satisfaction.
-• Worked with the Agile Methodology, which involved connecting with the customer to gather project requirements,
-refining the backlog, developing, testing, and releasing the product ",
+Worked with the Agile Methodology, which involved connecting with the customer to gather project requirements,
+refining the backlog, developing, testing, and releasing the product. ",
       color: "#0879bf",
     },
   ],
@@ -548,10 +548,10 @@ const experience = {
           duration: "June 2023 - Nov 2023",
           location: "Coimbatore TamilNadu",
           description:
-            "• Collaborated on developing a GUI for Evaluation Module (EVM) for Texas Instruments.
-• Successfully built an LED driver GUI using Polymer.js and enabled users to connect and test hardware with various
+            "Collaborated on developing a GUI for Evaluation Module (EVM) for Texas Instruments.
+Successfully built an LED driver GUI using Polymer.js and enabled users to connect and test hardware with various
 LED configurations and modes, resulting in positive customer feedback and increased client satisfaction.
-• Worked with the Agile Methodology, which involved connecting with the customer to gather project requirements,
+Worked with the Agile Methodology, which involved connecting with the customer to gather project requirements,
 refining the backlog, developing, testing, and releasing the product. ",
           color: "#0879bf",
         },
@@ -601,10 +601,9 @@ refining the backlog, developing, testing, and releasing the product. ",
           duration: "Jan 2023 - May 2023",
           location: "Coimbatore TamilNadu",
           description:
-            "• Developed responsive projects such as Weather Application, Tic-Tac-Toe Game Application, Shopping Cart, Calculator
+            "Developed responsive projects such as Weather Application, Tic-Tac-Toe Game Application, Shopping Cart, Calculator
 App and Chat App using Html, CSS, JavaScript, React.js, Node.js and Socket.io.
-• Implemented Unit Test cases using Jest framework and E2E Test cases using Playwright. Deployed the projects using
-Docker and hosted it using Glitch Website. ",
+Implemented Unit Test cases using Jest framework and E2E Test cases using Playwright. Deployed the projects using Docker and hosted it using Glitch Website. ",
           color: "#0879bf",
         },
         {
